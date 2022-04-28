c56a2800b0e7b6a5b445a04e3a61c07fb3daf0f8
prince_of_crows <tamjidjasir@gmail.com>
2022-04-28 22:23:33 +0600

    Add collected components.

---
+++ Graph/Connected components, Bridges, Articulations points/ConnectedComponents.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+#define MAXN 100005
+
+int n;
+vector<int> g[MAXN];
+bool used[MAXN];
+vector<int> comp;
+vector<vector<int> > compCol;
+
+void init(int n) {
+	for (int i = 0; i < n ; ++i) {
+		used [i] = false;
+	}
+}
+
+void dfs(int v) {
+	used[v] = true;
+	comp.push_back(v);
+
+	for (auto to: g[v]) {
+		if (!used[to]) {
+			dfs(to);
+		}
+
+	}
+}
+
+void findComps() {
+	init(n);
+
+	for (int i = 0; i < n ; ++i) {
+		if (!used[i]) {
+			comp.clear();
+			dfs(i);
+			compCol.push_back(comp);
+		}
+	}
+}
