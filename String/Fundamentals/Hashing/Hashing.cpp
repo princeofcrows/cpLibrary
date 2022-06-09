195f27afd1d81756a879ca9b922869ed76693f69
prince_of_crows <tamjidjasir@gmail.com>
2022-06-09 20:25:42 +0600

    Add hash function for strings.

---
+++ String/Fundamentals/Hashing/Hashing.cpp
@@ -0,0 +1,18 @@
+
+#include <string>
+using namespace std;
+
+
+long long computeHash(string const& s) {
+	const int p = 31;
+	const int m = 1e9 + 9;
+
+	long long hashValue = 0;
+	long long pPow = 1;
+
+	for (char c : s) {
+		hashValue = (hashValue + (c - 'a' + 1) * pPow) % m;
+		pPow = (pPow * p) % m;
+	}
+	return hashValue;
+}
