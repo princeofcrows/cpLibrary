b9a1ebfb476aefea219d4b3be0abb7fc242651ca
prince_of_crows <tamjidjasir@gmail.com>
2023-07-15 09:54:41 +0600

    Add template

---
+++ template.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Data type
+#define int long long
+#define ll long long
+#define pii pair <int, int>
+#define piii pair <int, pii>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define msi map<string, int>
+#define si set<int>
+
+// Method or var shorthands
+#define fi first
+#define se second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+#define sort_all(v) sort(all(v));
+#define rev_all(v) rev(all(v));
+#define mem(ara, x) memset(ara, x, sizeof ara)
+
+// Const
+#define mod 1000000007
+#define inf 1e18+19
+#define mx 200015
+#define pi acos(-1.0)
+
+// Input Output
+#define sild(x) scanf("%lld", &x)
+#define sid(x) scanf("%d", &x)
+#define rin() freopen("in.txt", "r", stdin)
+#define wrout() freopen("out.txt", "w", stdout)
+#define fst ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+// Loops
+#define fr(i,s,e) for(int i=s; i<e; i++)
+#define ifr(i,s,e) for(int i=s; i<=e; i++)
+#define rfr(i,e,s) for(int i=e-1; i>=s; i--)
+
+// Functions
+ll gcd(ll a,ll b) {
+	if (b==0) return a; return gcd(b, a%b);
+}
+
+ll lcm(ll a,ll b) {
+	return a/gcd(a,b)*b;
+}
+
+void yes() {
+	cout << "YES\n";
+}
+
+void no() {
+	cout << "NO\n";
+}
+
+int32_t main(){
+	//rin();
+	//wrout();
+	fst;
+
+	int t;
+	cin >> t;
+
+	while(t--) {
+
+	}
+	return 0;
+}
