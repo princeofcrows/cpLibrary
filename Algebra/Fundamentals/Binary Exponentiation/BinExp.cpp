b5644abd936e894d9825a828f97d96a5bf05220c
prince_of_crows <tamjidjasir@gmail.com>
2023-11-02 19:16:33 +0600

    Add Euler Totient

--- Algebra/Fundamentals/Binary Exponentiation/BinExp.cpp
+++ Algebra/Fundamentals/Binary Exponentiation/BinExp.cpp
@@ -1,25 +1,39 @@
-long long binPowRec(long long a, long long b) {
-	if (b == 0) return 1;
+#include <algorithm>
+#include <vector>
+
+#define mod 1000000007
+using namespace std;
+
+long long binPowRec(long long a, long long b)
+{
+	if (b == 0)
+		return 1;
 
 	long long res = binPowRec(a, b / 2);
 
-	if (b % 2) {
-		return res * res * a;
+	if (b % 2)
+	{
+		return (res * res * a) % mod;
 	}
-	else {
-		return res * res;
+	else
+	{
+		return (res * res) % mod;
 	}
 }
 
-long long binPowItr(long long a, long long b) {
+long long binPowItr(long long a, long long b)
+{
+
 	long long res = 1;
 
-	while (b > 0) {
-		if (b & 1) {
-			res = res * a;
+	while (b > 0)
+	{
+		if (b & 1)
+		{
+			res = (res * a) % mod;
 		}
 
-		a = a * a;
+		a = (a * a) % mod;
 		b >>= 1;
 	}
 	return res;
